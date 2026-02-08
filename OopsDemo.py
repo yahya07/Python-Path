3dc62c80587dde34c24c9d5951de8cc5a2ca2367
Yahya <ywahishi@site.sa>
2026-02-08 21:39:59 +0300

    Good Start

---
+++ OopsDemo.py
@@ -0,0 +1,21 @@
+class Calcolator:
+
+  num =100
+
+  def __init__(self,a,b):
+    self.first = a 
+    self.second  = b
+    print("this is from class side")
+
+  def getdata(self):
+    print("hello from function inside class")
+
+  def summition(self,a,b):
+    self.first + self.second
+    return
+
+obj = Calcolator(10,10)
+
+print(obj.getdata())
+
+  
